c5f9c004ecd4b7e7ea9c279d032ff37ed1e96925
rieske <vaidotas.valuckas@gmail.com>
2014-02-22 12:06:29 +0200

    initial basic configuration

---
+++ src/main/java/lt/rieske/mvc/rest/web/TestController.java
@@ -0,0 +1,16 @@
+package lt.rieske.mvc.rest.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String showIndex() {
+        return "Hello world";
+    }
+}
