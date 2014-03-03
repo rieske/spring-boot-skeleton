37a63442a13848bb71cb73c0725a79d8ff49f8bd
rieske <vaidotas.valuckas@gmail.com>
2014-03-03 06:38:05 +0200

    basic swagger docs configuration

--- src/main/java/lt/rieske/mvc/rest/config/Application.java
+++ src/main/java/lt/rieske/mvc/rest/config/Application.java
@@ -1,18 +1,23 @@
 package lt.rieske.mvc.rest.config;
 
-import lt.rieske.mvc.external.SwaggerApiDocsConfiguration;
-
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-
-@ComponentScan(basePackages={"lt.rieske.mvc.rest"})
+@Configuration
 @EnableAutoConfiguration
-public class Application {
+@ComponentScan(basePackages = { "lt.rieske.mvc.rest" })
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(Application.class);
+    }
 
     public static void main(String[] args) {
-        SpringApplicationBuilder application = new SpringApplicationBuilder();
-        application.sources(Application.class).child(Application.class, SwaggerApiDocsConfiguration.class).run(args);
+        new Application().configure(new SpringApplicationBuilder()).run(args);
     }
+
 }
