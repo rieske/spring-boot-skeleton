4a6a36e42f47e4588a35bd3b363b16b5ba33163a
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2019-11-07 10:52:36 +0200

    disable ryuk for travis

--- src/component-test/java/lt/rieske/skeleton/integration/ExampleEndpointTest.java
+++ src/component-test/java/lt/rieske/skeleton/integration/ExampleEndpointTest.java
@@ -9,26 +9,26 @@ import static org.hamcrest.Matchers.notNullValue;
 
 public class ExampleEndpointTest {
 
-	@Test
-	public void returnsDomainObjectWithGivenId() {
-		// @formatter:off
+    @Test
+    public void returnsDomainObjectWithGivenId() {
+        // @formatter:off
 		when().
 			get("/api/object/abcde54321").
 		then().
 			statusCode(200).
 			body(equalTo("{\"id\":\"abcde54321\"}"));
 		// @formatter:on
-	}
+    }
 
-	@Test
-	public void returnsRandomDomainObject() {
-		// @formatter:off
+    @Test
+    public void returnsRandomDomainObject() {
+        // @formatter:off
 		when().
 			get("/api/random").
 		then().
 			statusCode(200).
 			body(notNullValue());
-		// @formatter:o
-	}
-	
+		// @formatter:on
+    }
+
 }
